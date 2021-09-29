efa77c26fa202853bf00617371de468f35dbbc0e
acbka <vladochka.vb@gmail.com>
2021-09-30 12:04:50 +1300

    fix issue

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,3 +1,3 @@
 export const horsesUrl = "https://domvetnz.herokuapp.com/horses";
 export const horseByIdUrl = (id: string) =>
-  `http://domvetnz.herokuapp.com/horses/${id}`;
+  `https://domvetnz.herokuapp.com/horses/${id}`;
