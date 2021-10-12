63c7fbe1bbcdb7a1561623f31f273b8795806056
acbka <vladochka.vb@gmail.com>
2021-10-12 15:56:12 +1300

    create array of horse's breeds

---
+++ src/common/horseBreeds.ts
@@ -0,0 +1,12 @@
+export const horseBreeds = [
+  "American Quarter Horse",
+  "Arabian",
+  "Thoroughbred",
+  "Warmblood",
+  "Morgan",
+  "Appaloosa",
+  "Pony",
+  "Tennessee Walker",
+  "Paint",
+  "Andalusian",
+];
